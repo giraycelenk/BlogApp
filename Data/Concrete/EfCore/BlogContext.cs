0024afc2bb12cefd88c49b46ad2f526d20539d18
giraycelenk <a.giraycelenk@hotmail.com>
2024-09-15 20:08:11 +0300

    database connections

--- Data/Concrete/EfCore/BlogContext.cs
+++ Data/Concrete/EfCore/BlogContext.cs
@@ -1,7 +1,16 @@
-namespace BlogApp.Data.Concrete.EfCore;
+using Microsoft.EntityFrameworkCore;
+using BlogApp.Entity;
+namespace BlogApp.Data.Concrete.EfCore
 {
-    public class BlogContext
+    public class BlogContext:DbContext
     {
-
+        public BlogContext(DbContextOptions<BlogContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Post> Posts => Set<Post>();
+        public DbSet<Comment> Comments => Set<Comment>();
+        public DbSet<Tag> Tags => Set<Tag>();
+        public DbSet<User> Users => Set<User>();
     }
 }
